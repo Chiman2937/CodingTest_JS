d73dd08a1c0d23ea8cf777e1bfb2328cb48940fd
Chiyoung <160126901+Chiman2937@users.noreply.github.com>
2025-09-21 22:58:25 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.82 ms, Memory: 33 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -0,0 +1,9 @@
+const solution = (A, B) => {
+  A.sort((a, b) => a - b);
+  B.sort((a, b) => b - a);
+  let result = 0;
+  for (i = 0; i < A.length; i++) {
+    result += A[i] * B[i];
+  }
+  return result;
+};
