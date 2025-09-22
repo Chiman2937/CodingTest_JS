8b0ba83bbc0947e8694a1a6146cffbf661c43461
Chiyoung <160126901+Chiman2937@users.noreply.github.com>
2025-09-22 15:27:53 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,9 +1,5 @@
 function solution(n, arr1, arr2) {
     let answer = [];
-    for(i=0;i<n;i++){
-        const bin = (arr1[i] | arr2[i]).toString(2);
-        const next = bin.replaceAll("1","#").replaceAll("0"," ").padStart(n," ");
-        answer.push(next);
-    }
-    return answer;
+    const arr = arr1.map((v,i) => (v | arr2[i]).toString(2).replaceAll('1', "#").replaceAll('0', " ").padStart(n, " "));
+    return arr;
 }
