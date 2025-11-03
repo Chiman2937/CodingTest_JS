1eeb45302141154c15935f2c540d0e2aa5125dd5
Chiyoung <160126901+Chiman2937@users.noreply.github.com>
2025-11-03 22:33:07 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,3 +1,3 @@
-const solution = (s) => {
-    return s.split(' ').map((v)=>v.substring(0,1).toUpperCase() + v.substring(1).toLowerCase()).join(' ')
+function solution(s) {
+    return s.split(' ').map((v)=>v.substring(0,1).toUpperCase()+v.substring(1).toLowerCase()).join(' ')
 }
