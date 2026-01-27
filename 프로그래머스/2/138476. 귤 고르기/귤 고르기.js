dc2c74f298caac7188f8ec6c2b7610126976ca11
Chiyoung <160126901+Chiman2937@users.noreply.github.com>
2026-01-27 11:14:55 +0900

    [level 2] Title: 귤 고르기, Time: 56.36 ms, Memory: 56.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -0,0 +1,15 @@
+function solution(k, tangerine) {
+    const listMap = new Map();
+    for(const t of tangerine){
+        listMap.set(t,(listMap.get(t)||0)+1);
+    }
+    const listArray = [...listMap].sort((a,b)=>b[1]-a[1]);
+    let count = 0;
+    let index = 0;
+    console.log(listArray);
+    while(count<k){
+        count+=listArray[index][1];
+        index++;
+    }
+    return index;
+}
