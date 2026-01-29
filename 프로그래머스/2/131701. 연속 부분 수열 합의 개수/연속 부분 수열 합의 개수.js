ef37663b4ffcec60b7b49984666754fb5cfee351
Chiyoung <160126901+Chiman2937@users.noreply.github.com>
2026-01-29 10:28:53 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 74.83 ms, Memory: 57 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -2,14 +2,10 @@ function solution(elements) {
     let result = new Set();
     const size = elements.length;
     elements = [...elements,...elements];
-    // 순열 길이만큼 순회
-    for(let l = 1; l <= size; l++){
-        // elements의 길이만큼 순회
-        for(let i = 0; i < size; i++){
-            let sum = 0;
-            for(let index = i; index <i+l ; index++){
-                sum+=elements[index];
-            }
+    for(let i = 0; i< size; i++){
+        let sum = 0;
+        for(let l =0; l< size;l++){
+            sum+=elements[i+l];
             result.add(sum);
         }
     }
