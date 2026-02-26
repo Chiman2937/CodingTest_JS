176f4a962f407697835468368185a64dd52c6d06
Chiyoung <160126901+Chiman2937@users.noreply.github.com>
2026-02-26 10:48:16 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 4.19 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.js
@@ -0,0 +1,11 @@
+function solution(storey) {
+    let result = 0;
+    while(storey > 0){
+        const digit = String(storey).length
+        const count = Math.round((storey-0.1) / 10**(digit - 1));
+        result += count >= 6 ? 10 - count + 1 : count;
+        storey = Math.abs(count * 10**(digit-1) - storey);
+				console.log(storey, count, result)
+    }
+    return result;
+}
