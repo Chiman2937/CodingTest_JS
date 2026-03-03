d877113a38cb00a0e7d834c6aa55e8f37e0025c6
Chiyoung <160126901+Chiman2937@users.noreply.github.com>
2026-03-03 17:10:00 +0900

    Time: 16 ms (17.72%), Space: 62.7 MB (18.84%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function(s, numRows) {
+    let arrs = new Array(numRows).fill(0).map((v)=>[])
+    let zigzagMap = [numRows-1];
+    while(zigzagMap[0] > 1){
+        const currentNumber = zigzagMap[0];
+        zigzagMap.push(currentNumber-1);
+        zigzagMap.unshift(currentNumber-1);
+    }
+    zigzagMap.unshift(0);
+
+    const zigzagLength = zigzagMap.length;
+    for(let i = 0; i < s.length; i++){
+        arrs[zigzagMap[i % zigzagLength]].push(s[i])
+    }
+    return arrs.flat().join('')
+};
