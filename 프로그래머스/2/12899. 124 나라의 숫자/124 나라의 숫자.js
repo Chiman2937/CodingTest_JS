42beda4032836ef63622809b0f8e00ae3c302d1a
Chiyoung <160126901+Chiman2937@users.noreply.github.com>
2026-03-03 11:54:24 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.19 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.js
+++ 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.js
@@ -3,11 +3,10 @@ function solution(n) {
     while(n > 0){
         if(n%3 === 0){
             answer="4"+answer
-            n=n/3 - 1
         } else {
             answer=(n%3) + answer
-            n=~~(n/3)
         }
+        n=Math.ceil(n/3)-1
     }
     return answer;
 }
