d224ad42532c184199c948f5f5ac7109b0cc9183
Chiyoung <160126901+Chiman2937@users.noreply.github.com>
2026-03-04 18:04:15 +0900

    Time: 0 ms (100%), Space: 59.1 MB (61.19%) - LeetHub

--- LeetCode/0055-jump-game/0055-jump-game.js
+++ LeetCode/0055-jump-game/0055-jump-game.js
@@ -3,15 +3,25 @@
  * @return {boolean}
  */
 var canJump = function(nums) {
-    const n = nums.length;
-    let jumps = new Array(n).fill(false);
-    jumps[0] = true;
+    // // DP 방식
+    // const n = nums.length;
+    // let jumps = new Array(n).fill(false);
+    // jumps[0] = true;
 
-    for(let i = 0; i < n;i++){
-        for(let j = 1; j <= nums[i] ; j++) {
-            if(i+j >= n) continue
-            if(jumps[i]) jumps[i+j] = true
-        }
+    // for(let i = 0; i < n;i++){
+    //     for(let j = 1; j <= nums[i] ; j++) {
+    //         if(i+j >= n) continue
+    //         if(jumps[i]) jumps[i+j] = true
+    //     }
+    // }
+    // return jumps[n-1]
+
+    // Greedy 방식
+    const n = nums.length;
+    let farthest = 0;
+    for(let i = 0; i < n-1; i++){
+        farthest = Math.max(farthest, i + nums[i]);
+        if(farthest === i && nums[i] === 0) return false
     }
-    return jumps[n-1]
+    return true;
 };
