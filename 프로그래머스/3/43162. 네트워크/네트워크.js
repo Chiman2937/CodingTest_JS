9ab40152a0ebe8b6e1eabd23ae691a51e0fb9204
Chiyoung <160126901+Chiman2937@users.noreply.github.com>
2026-03-04 19:34:11 +0900

    [level 3] Title: 네트워크, Time: 7.98 ms, Memory: 34.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -0,0 +1,28 @@
+function solution(n, computers) {
+    let graph = new Map();
+    for(let i = 0; i< computers.length; i++){
+        graph.set(i,[])
+        for(let j = 0; j < computers[i].length ; j++){
+            if(i===j) continue
+            if(computers[i][j] === 1) graph.get(i).push(j)
+        }
+    }
+    let visitors = new Set();
+    let queue = [];
+    let count = 0;
+    for(let i = 0; i< computers.length; i++){
+        queue.push(i);
+        if(!visitors.has(i)) count++;
+        while(queue.length > 0){
+            let key = queue.shift();
+            if(visitors.has(key)) continue
+            visitors.add(key);
+            let members = graph.get(key);
+            console.log(members)
+            for(const m of members){
+                queue.push(m);
+            }
+        }
+    }
+    return count;
+}
