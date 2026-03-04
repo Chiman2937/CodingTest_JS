9ce1748a88b7980c558f811dfb966cee7fab4490
Chiyoung <160126901+Chiman2937@users.noreply.github.com>
2026-03-04 16:54:25 +0900

    Time: 1 ms (84.81%), Space: 55.2 MB (40.24%) - LeetHub

--- LeetCode/0045-jump-game-ii/0045-jump-game-ii.js
+++ LeetCode/0045-jump-game-ii/0045-jump-game-ii.js
@@ -3,16 +3,30 @@
  * @return {number}
  */
 var jump = function(nums) {
-    const n = nums.length;
-    // 초기화
-    let jump = new Array(n).fill(Infinity);
-    jump[0] = 0;
+    // DP 방식, O(N^2)
+    // const n = nums.length;
+    // // 초기화
+    // let jump = new Array(n).fill(Infinity);
+    // jump[0] = 0;
     
-    for(let i = 0; i< n; i++){
-        for(let j = 1; j <= nums[i]; j++){
-            jump[i+j] = Math.min(jump[i] + 1, jump[i+j])
-            if (i+j === n-1) return jump[i+j];
+    // for(let i = 0; i< n; i++){
+    //     for(let j = 1; j <= nums[i]; j++){
+    //         jump[i+j] = Math.min(jump[i] + 1, jump[i+j])
+    //         if (i+j === n-1) return jump[i+j];
+    //     }
+    // }
+    // return 0
+
+    // Greedy 방식, O(N)
+    let jumps = 0;
+    let end = 0;
+    let farthest = 0;
+    for(let i = 0; i < nums.length - 1; i++){
+        farthest = Math.max(farthest,i+nums[i]);
+        if(i === end){
+            end = farthest;
+            jumps++;
         }
     }
-    return 0
+    return jumps;
 };
