f11743f5923ee0fd80925b4edd87302eb1503f92
Chiyoung <160126901+Chiman2937@users.noreply.github.com>
2026-03-16 11:19:51 +0900

    [level 2] Title: 시소 짝꿍, Time: 46.81 ms, Memory: 40.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.js
@@ -0,0 +1,26 @@
+function solution(weights) {
+    let result = 0;
+    let ratios = [
+        [1,1],
+        [2,3],
+        [3,2],
+        [2,4],
+        [4,2],
+        [3,4],
+        [4,3]
+    ]
+    
+    let map = new Map();
+    
+    for (const w of weights){
+        for(const [d1,d2] of ratios) {
+            if((w * d2) % d1 === 0){
+                const partner = (w * d2) / d1;
+                result+=(map.get(partner)) ?? 0
+            }
+        }
+        map.set(w, (map.get(w) ?? 0) + 1)
+    }
+    return result;
+}
+
