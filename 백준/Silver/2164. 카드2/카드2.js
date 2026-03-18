0ca423de46f09f6acd91b1ff73135393f2e49c6f
Chiyoung <160126901+Chiman2937@users.noreply.github.com>
2026-03-18 12:20:29 +0900

    [Silver IV] Title: 카드2, Time: 160 ms, Memory: 11828 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.js
+++ 백준/Silver/2164. 카드2/카드2.js
@@ -8,18 +8,23 @@ let input = require('fs')
 const n = Number(input[0]);
 
 function solution(n) {
-  let queue = new Array(n).fill(0).map((_, i) => i + 1);
+  let array = new Int32Array(n + 1);
 
-  let head = 0;
-
-  while (head < queue.length - 1) {
-    head++;
-    const next = queue[head];
-    head++;
-    queue.push(next);
+  for (let i = 1; i <= n; i++) {
+    array[i] = (i % n) + 1;
   }
 
-  return queue[head];
+  let remain = n; // 남은 카드 갯수
+  let cur = n; // 버릴카드를 가리키는 카드가 되어야함
+
+  while (remain > 1) {
+    let toDiscard = array[cur];
+    let toMove = array[toDiscard];
+    array[cur] = toMove;
+    cur = toMove;
+    remain--;
+  }
+  return array[cur];
 }
 
 console.log(solution(n));
