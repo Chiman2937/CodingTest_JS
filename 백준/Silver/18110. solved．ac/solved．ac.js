8bdf51adca984cf44d491f8fde2ffb3843d631bb
Chiyoung <160126901+Chiman2937@users.noreply.github.com>
2026-03-22 12:32:59 +0900

    [Silver IV] Title: solved.ac, Time: 348 ms, Memory: 66728 KB -BaekjoonHub

--- 백준/Silver/18110. solved．ac/solved．ac.js
+++ 백준/Silver/18110. solved．ac/solved．ac.js
@@ -5,19 +5,14 @@ let input = require('fs')
   .trim()
   .split(/\r?\n/);
 
-let [n, ...numbers] = input;
-n = Number(n);
-numbers = numbers.map(Number);
+let [n, ...list] = input.map(Number);
 
-function solution(n, numbers) {
-  if (n === 0) return 0;
-
-  numbers = numbers.sort((a, b) => a - b);
+function solution(n, list) {
+  if (list.length === 0) return 0;
+  list = list.sort((a, b) => a - b);
   let cut = Math.round(n * 0.15);
-
-  numbers = numbers.slice(cut, n - cut);
-
-  return Math.round(numbers.reduce((a, c) => a + c, 0) / numbers.length);
+  list = list.slice(cut, n - cut);
+  return Math.round(list.reduce((a, c) => a + c, 0) / (n - cut * 2));
 }
 
-console.log(solution(n, numbers));
+console.log(solution(n, list));
