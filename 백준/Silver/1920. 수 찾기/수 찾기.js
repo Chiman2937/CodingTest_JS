5755d95e956e1ee6cdbd0e71e681298774738e21
Chiyoung <160126901+Chiman2937@users.noreply.github.com>
2026-03-22 11:37:16 +0900

    [Silver IV] Title: 수 찾기, Time: 272 ms, Memory: 47876 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.js
+++ 백준/Silver/1920. 수 찾기/수 찾기.js
@@ -5,32 +5,26 @@ let input = require('fs')
   .trim()
   .split(/\r?\n/);
 
-const [n, members_n, m, members_m] = input;
+let [n, dic, m, find] = input;
 
-function solution(n, members_n, m, members_m) {
-  members_n = members_n
-    .split(' ')
-    .map(Number)
-    .sort((a, b) => a - b);
-  members_m = members_m.split(' ').map(Number);
+dic = dic.split(' ').map(Number);
+find = find.split(' ').map(Number);
+
+function solution(n, dic, m, find) {
+  let map = new Map();
   let result = '';
-
-  for (const number of members_m) {
-    let left = 0;
-    let right = n;
-    while (left < right) {
-      let mid = ~~((left + right) / 2);
-      if (number < members_n[mid]) right = mid;
-      else if (number > members_n[mid]) left = mid + 1;
-      else if (number === members_n[mid]) {
-        result += '1\n';
-        break;
-      }
-    }
-    if (left >= right) result += '0\n';
+  for (let d of dic) {
+    map.set(d, 1);
   }
-
+  for (let f of find) {
+    let isExist = false;
+    if (map.get(f)) {
+      result += '1\n';
+      continue;
+    }
+    result += '0\n';
+  }
   return result;
 }
 
-console.log(solution(Number(n), members_n, Number(m), members_m));
+console.log(solution(n, dic, m, find));
