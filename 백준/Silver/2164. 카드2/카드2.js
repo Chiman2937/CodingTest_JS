f42091de5de3b966ece83b88d9e38c5ea366251a
Chiyoung <160126901+Chiman2937@users.noreply.github.com>
2026-03-22 13:56:13 +0900

    [Silver IV] Title: 카드2, Time: 172 ms, Memory: 17952 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.js
+++ 백준/Silver/2164. 카드2/카드2.js
@@ -8,11 +8,20 @@ let input = require('fs')
 let n = +input[0];
 
 function solution(n) {
-  let list = new Array(n).fill(0).map((v, i) => i + 1);
-  for (let i = 1; i < list.length; i += 2) {
-    list.push(list[i]);
+  let list = new Array(n + 1)
+    .fill(0)
+    .map((v, i) => (i === 0 ? 0 : (i % n) + 1));
+
+  let remain = n;
+  let cur = n;
+  while (remain > 1) {
+    let toDiscard = list[cur];
+    let toMove = list[toDiscard];
+    list[cur] = toMove;
+    cur = toMove;
+    remain--;
   }
-  return list[list.length - 1];
+  return list[cur];
 }
 
 console.log(solution(n));
