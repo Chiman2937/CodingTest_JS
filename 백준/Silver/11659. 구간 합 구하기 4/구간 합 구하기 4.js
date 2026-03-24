f0a9e2181207b2560a49207983d5208d6fd00c0c
Chiyoung <160126901+Chiman2937@users.noreply.github.com>
2026-03-24 14:33:35 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 396 ms, Memory: 65524 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.js
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.js
@@ -11,14 +11,14 @@ numbers = numbers.split(' ').map(Number);
 list = list.map((v) => v.split(' ').map(Number));
 
 function solution(n, m, numbers, list) {
+  numbers.unshift(0);
   let result = '';
-  let sum = new Array(n).fill(0);
-  sum[0] = numbers[0];
-  for (let i = 1; i < n; i++) {
+  let sum = new Array(n + 1).fill(0);
+  for (let i = 1; i <= n; i++) {
     sum[i] = sum[i - 1] + numbers[i];
   }
   for (const s of list) {
-    result += sum[s[1] - 1] - (sum[s[0] - 2] ?? 0) + '\n';
+    result += sum[s[1]] - sum[s[0] - 1] + '\n';
   }
   return result;
 }
