e4ffa93b6e1f567badc621713f3694ebb1d188aa
Chiyoung <160126901+Chiman2937@users.noreply.github.com>
2026-03-26 10:30:24 +0900

    [Silver II] Title: 색종이 만들기, Time: 168 ms, Memory: 12864 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.js
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.js
@@ -11,36 +11,31 @@ n = +n;
 board = board.map((v) => v.split(' ').map(Number));
 
 function solution(n, board) {
-  let result = [0, 0];
-
-  function rec(board) {
-    if (board.flat().every((v) => v === 0)) {
-      result[0]++;
-      return;
-    }
-    if (board.flat().every((v) => v === 1)) {
-      result[1]++;
-      return;
-    }
-    const n = board.length;
-    let dirs = [
-      [0, 0],
-      [0, n / 2],
-      [n / 2, 0],
-      [n / 2, n / 2],
-    ];
-    for (let d of dirs) {
-      let next = new Array(n / 2).fill(0).map(() => new Array(n / 2));
-      for (let i = 0; i < n / 2; i++) {
-        for (let j = 0; j < n / 2; j++) {
-          next[i][j] = board[i + d[0]][j + d[1]];
+  function rec(r, c, size) {
+    const first = board[r][c];
+    let isUniform = true;
+    outer: for (let i = r; i < r + size; i++) {
+      for (let j = c; j < c + size; j++) {
+        if (first !== board[i][j]) {
+          isUniform = false;
+          break outer;
         }
       }
-      rec(next);
     }
+    if (isUniform) return [first === 0 ? 1 : 0, first === 1 ? 1 : 0];
+
+    const half = size / 2;
+
+    const sub = [
+      rec(r, c, half),
+      rec(r + half, c, half),
+      rec(r, c + half, half),
+      rec(r + half, c + half, half),
+    ];
+    return sub.reduce(([w, b], [cw, cb]) => [w + cw, b + cb]);
   }
-  rec(board);
-  return result.join('\n');
+  const [white, blue] = rec(0, 0, n);
+  return white + '\n' + blue;
 }
 
 console.log(solution(n, board));
