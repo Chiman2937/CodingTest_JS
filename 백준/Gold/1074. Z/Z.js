db6d98083768168e0f7846577b21dce539af279f
Chiyoung <160126901+Chiman2937@users.noreply.github.com>
2026-03-27 12:49:52 +0900

    [Gold V] Title: Z, Time: 96 ms, Memory: 9600 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.js
+++ 백준/Gold/1074. Z/Z.js
@@ -8,22 +8,16 @@ let input = require('fs')
 let [n, r, c] = input[0].split(' ').map(Number);
 
 function solution(n, r, c) {
-  let size = 2 ** n;
+  if (n === 0) return 0;
+  let half = 2 ** (n - 1);
+  let halfSquareSize = half * half;
 
-  function recursion(size, cr, cc, distance) {
-    if (size === 1 && cr === r && cc === c) {
-      return distance;
-    }
-    let half = size / 2;
-    let nr = cr + half > r ? cr : cr + half;
-    let nc = cc + half > c ? cc : cc + half;
-    let nDistance =
-      distance +
-      (cr + half > r ? 0 : half ** 2 * 2) +
-      (cc + half > c ? 0 : half ** 2);
-    return recursion(half, nr, nc, nDistance);
-  }
-  return recursion(size, 0, 0, 0);
+  if (r < half && c < half) return solution(n - 1, r, c);
+  else if (r < half && c >= half)
+    return halfSquareSize + solution(n - 1, r, c - half);
+  else if (r >= half && c < half)
+    return halfSquareSize * 2 + solution(n - 1, r - half, c);
+  return halfSquareSize * 3 + solution(n - 1, r - half, c - half);
 }
 
 console.log(solution(n, r, c));
