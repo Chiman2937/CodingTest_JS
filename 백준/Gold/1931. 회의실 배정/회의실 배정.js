718dae8adefd03f8fc13b5b0ee7321d8d0355839
Chiyoung <160126901+Chiman2937@users.noreply.github.com>
2026-03-28 18:07:41 +0900

    [Gold V] Title: 회의실 배정, Time: 384 ms, Memory: 47844 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.js
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.js
@@ -9,10 +9,7 @@ let [n, ...meetings] = input;
 meetings = meetings.map((v) => v.split(' ').map(Number));
 
 function solution(n, meetings) {
-  meetings.sort((a, b) => {
-    if (a[1] === b[1]) return a[0] - b[0];
-    return a[1] - b[1];
-  });
+  meetings.sort((a, b) => a[1] - b[1] || a[0] - b[0]);
   let result = 0;
   let currentTime = 0;
   for (const [s, f] of meetings) {
