d7d4c22841f4c49faaf87ad3d2dd88f506bfcf7b
Chiyoung <160126901+Chiman2937@users.noreply.github.com>
2026-03-29 21:25:27 +0900

    [Gold IV] Title: 테트로미노, Time: 716 ms, Memory: 23836 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.js
+++ 백준/Gold/14500. 테트로미노/테트로미노.js
@@ -5,86 +5,67 @@ let input = require('fs')
   .trim()
   .split(/\r?\n/);
 
-let [num, ...board] = input;
+let [num, ...list] = input;
 let [n, m] = num.split(' ').map(Number);
-board = board.map((v) => v.split(' ').map(Number));
+let board = list.map((v) => v.split(' ').map(Number));
 
 function solution(n, m, board) {
   let isVisited = new Array(n).fill(0).map(() => new Array(m).fill(false));
+
   let dirs = [
     [0, 1],
     [1, 0],
     [0, -1],
     [-1, 0],
   ];
-  function dfs(row, col, sum, count) {
-    // console.log(row, col, sum, count);
+
+  function dfs(r, c, sum, count) {
     if (count === 4) {
       return sum;
     }
     let result = sum;
     for (let [dr, dc] of dirs) {
-      let nextRow = row + dr;
-      let nextCol = col + dc;
-      if (nextRow >= 0 && nextRow < n && nextCol >= 0 && nextCol < m) {
-        if (isVisited[nextRow][nextCol]) continue;
-        isVisited[nextRow][nextCol] = true;
-        result = Math.max(
-          result,
-          dfs(nextRow, nextCol, sum + board[nextRow][nextCol], count + 1),
-        );
-        isVisited[nextRow][nextCol] = false;
+      let nr = r + dr;
+      let nc = c + dc;
+      if (nr >= 0 && nr < n && nc >= 0 && nc < m) {
+        if (!isVisited[nr][nc]) {
+          isVisited[nr][nc] = true;
+          result = Math.max(
+            result,
+            dfs(nr, nc, sum + board[nr][nc], count + 1),
+          );
+          isVisited[nr][nc] = false;
+        }
       }
     }
     return result;
   }
 
-  let result = 0;
-
-  for (let row = 0; row < n; row++) {
-    for (let col = 0; col < m; col++) {
-      // console.log(`start: ${row},${col}`);
-      isVisited[row][col] = true;
-      result = Math.max(result, dfs(row, col, board[row][col], 1));
-      isVisited[row][col] = false;
-      // console.log(`start: ${row},${col} : ㅏ 모양`);
-      if (row - 1 >= 0 && row + 1 < n) {
-        isVisited[row - 1][col] = true;
-        isVisited[row + 1][col] = true;
-        isVisited[row][col] = true;
-        result = Math.max(
-          result,
-          dfs(
-            row,
-            col,
-            board[row][col] + board[row - 1][col] + board[row + 1][col],
-            3,
-          ),
-        );
-        isVisited[row - 1][col] = false;
-        isVisited[row + 1][col] = false;
-        isVisited[row][col] = false;
-      }
-      // console.log(`start: ${row},${col} : ㅗ 모양`);
-      if (col - 1 >= 0 && col + 1 < m) {
-        isVisited[row][col - 1] = true;
-        isVisited[row][col + 1] = true;
-        isVisited[row][col] = true;
-        result = Math.max(
-          result,
-          dfs(
-            row,
-            col,
-            board[row][col] + board[row][col - 1] + board[row][col + 1],
-            3,
-          ),
-        );
-        isVisited[row][col - 1] = false;
-        isVisited[row][col + 1] = false;
-        isVisited[row][col] = false;
+  function tShape(r, c, base) {
+    let sumList = [];
+    for (let [dr, dc] of dirs) {
+      let nr = r + dr;
+      let nc = c + dc;
+      if (nr >= 0 && nr < n && nc >= 0 && nc < m) {
+        sumList.push(board[nr][nc]);
       }
     }
+    if (sumList.length < 3) return 0;
+    sumList.sort((a, b) => b - a);
+    return base + sumList.reduce((a, c, i) => a + (i < 3 ? c : 0), 0);
   }
+
+  let result = 0;
+
+  for (let r = 0; r < n; r++) {
+    for (let c = 0; c < m; c++) {
+      isVisited[r][c] = true;
+      result = Math.max(result, dfs(r, c, board[r][c], 1));
+      isVisited[r][c] = false;
+      result = Math.max(result, tShape(r, c, board[r][c]));
+    }
+  }
+
   return result;
 }
 
